bc02cbb1cb26d638070e990a2442b7ea45154113
renanbno <renanepitacio@gmail.com>
2024-04-16 10:05:17 -0300

    salvando

--- pizzaria.sql
+++ pizzaria.sql
@@ -1,44 +1,45 @@
--- Exibir todas base de dados
+-- Exibir todas as base de dados
 -- show databases;
 
-
--- drop database if exists pizzaria;
+-- apagar a base de dados caso exista
+drop database if exists pizzaria;
 
 create database pizzaria;
 
 use pizzaria;
 
-create table cliente (
-	cliente_id integer auto_increment not null,
-	telefone varchar (10),
-	nome varchar (30),
-	logradouro varchar (30),
-	numero decimal (5,0),
-	complemento varchar (30),
-	bairro varchar (30),
-	referencia varchar (30),
+create table cliente(
+	cliente_id int auto_increment not null,
+	telefone varchar(10),
+	nome varchar(30),
+	logradouro varchar(30),
+	numero decimal(5,0),
+	complemento varchar(30),
+	bairro varchar(30),
+	referencia varchar(30),
 	primary key (cliente_id)
 );
 
 create table pizza (
-	pizza_id integer auto_increment not null,
-	nome varchar (30),
-	descricao varchar (30),
-	valor decimal (4,2),
+	pizza_id int not null auto_increment,
+	nome varchar(30),
+	descrircao varchar(30),
+	valor decimal(4,2),
 	primary key (pizza_id)
 );
 
 create table pedido (
-	pedido_id integer auto_increment not null,
+	pedido_id int not null auto_increment,
 	cliente_id int,
-	data decimal(4,2),
+	data datetime,
+	valor decimal(4,2),
 	primary key (pedido_id),
 	constraint fk_cliente_pedido
 	foreign key (cliente_id)
 	references cliente (cliente_id)
 );
 
-	create table itens_pedido (
+create table item_pedido (
 	pedido_id int,
 	pizza_id int,
 	quantidade int,
@@ -50,13 +51,17 @@ create table pedido (
 		constraint fk_item_pizza
 	foreign key (pizza_id)
 	references pizza(pizza_id)
-	);
+);
 
-	-- show tables;
-	-- describe cliente;
-	-- alter table cliente drop telefone;
-	-- alter table cliente add telefone varchar(14);
-	alter table cliente modify telefone varchar(14);
+
+-- show tables;
+-- describe cliente ;
+
+-- alter table cliente drop telefone;
+
+-- alter table cliente add telefone varchar(14);
+
+alter table cliente modify telefone varchar(14);
 
 -- Inserindo registros na tabela de cliente
 INSERT INTO cliente (telefone, nome, logradouro, numero, complemento, bairro, referencia) VALUES ('(11) 1111-1111', 'Alexandre Santos', 'Rua das Palmeiras', 111, NULL, 'Bela Vista', 'Em frente a escola');
@@ -72,11 +77,10 @@ INSERT INTO pizza (nome, valor) VALUES ('Provolone', 17);
 INSERT INTO pizza (nome, valor) VALUES ('4 Queijos', 20);
 INSERT INTO pizza (nome, valor) VALUES ('Calabresa', 17);
 INSERT INTO pizza (nome) VALUES ('Escarola');
+
 select * from pizza;
-alter table pizza modify valor decimal (4,2) default 99.99;
-INSERT INTO pizza (nome) VALUES ('Moda da casa');
-
-
+alter table pizza modify valor decimal(4,2) default 99.99;
+insert into pizza (nome) values ('Moda da casa');
 -- Inserindo dados na tabela de pedido
 INSERT INTO pedido (pedido_id, cliente_id, data, valor) VALUES (1, 1, '2016-12-15 20:30:00', 32.00);
 INSERT INTO pedido (pedido_id, cliente_id, data, valor) VALUES (2, 2, '2016-12-15 20:38:00', 40.00);
@@ -127,35 +131,120 @@ INSERT INTO item_pedido (pedido_id, pizza_id, quantidade, valor) VALUES (8, 4, 2
     |   <=>   | Igual para NULL	 |
     ------------------------------
 */
- -- listar todas as pizzas
+
+-- listar todas as pizzas
 select * from pizza;
 
--- selecionar todas as pizzas com valor diferente de R#17,00
-select * from pizza where valor != 17;
-select * from pizza where valor <>= 17;
+-- selecionar todas as pizzas com valor diferente de R$ 17,00
 
--- selecionar todas as pizzas que não tem valor cadastrado valor = null
+select * from pizza where valor  !=17;
+select * from pizza where valor  <> 17;
+
+-- selecionar as pizzas que não tem valor cadastratdo valor = null
+
 select * from pizza where valor <=> null;
 select * from pizza where valor is null;
-select * from pizza where isnull(valor);
 
--- selecionar todas as pizzas com valores entre R$15,00 e R$20,00
+-- selecionar todas as pizzas com valores entre R$ 15,00 e R$20,00
+
 select * from pizza where valor > 15 and valor < 20;
 
--- selecionar todas as pizzas com valores menores ou maiores de R$17,00
-select * from pizza where valor < 17 or valor > 17;
+-- selecionar todas as pizzas com valores menores ou maiores que R$17,00
 
--- selecionar todas as pizzas com valores não menores ou maiores de R$17,00
-select * from pizza where not (valor < 17 or valor > 17);
+select * from pizza where valor < 17 or  valor > 17;
 
--- selecionar todas as pizzas com valores na faixa R$17,00 e R$20,00
-select * from pizza where  valor >= 17 and valor <= 20;
-select * from pizza where  valor between 17 and 20;
+-- selecionar todas as pizzas com valores não menores ou maiores que R$17,00
 
--- selecionar todas as pizzas com valores fora na faixa R$17,00 e R$20,00
+select * from pizza where not (valor < 17 or valor >  17 );
+
+-- selecionar todas as pizzas com valores na faixa R$ 17,00 e R$20,00
+
+select*from pizza where valor >= 17 and valor <=20;
+select * from pizza where valor between 17 and 20;
+
+-- selecionar todas as pizzas com valores fora da faixa de R$17 a R$20,00
+
 select * from pizza where valor < 17 or valor > 20;
-select * from pizza where not (valor >= 17 and valor <= 20);
-select * from pizza where  valor not between 17 and 20;
+select * from pizza where not (valor >=  17 and valor <= 20);
+select * from pizza where valor not between 17 and 20;
+
+-- selecionar todas as pizzas com valores a R$15 e R$20,00
+
+select * from pizza where valor < 15 or valor = 20;
+select * from pizza where valor in (15,20);
+
+-- selecionar todas as pizzas com valores diferentes de R$15 e R$20,00
+
+select * from pizza where valor < 15 end valor != 20;
+select * from pizza where valor not in (15,20);
+
+-- selecionar todas as pizzas com valores nullos
+select * from pizza where valor <=> null;
+select * from pizza where valor is null;
+
+-- selecionar todas as pizzas com valores diferentes de nullos
+
+select * from pizza where valor is not null;
+
+-- selecionar todas as pizzas que comecem com a letra E 
+select * from pizza where nome like 'E';
+
+-- selecionar todas as pizzas que terminam com a letra A
+select * from pizza where nome like '%a';
+
+-- selecionar todas as pizzas que contenha no nome a letra RO
+
+select * from pizza where nome like '%ro%';
+
+-- ordenar coluna
+
+-- desc esc -> opções de ordenação
+select * from pizza order by valor desc, nome;
+
+-- selecionar  as 3 mais caras 
+select * from pizza order by valor desc limit 4;
+
+-/* Funções de agregação 
+*AVG(coluna) Média dos calores da coluna 
+*count (coluna) Conta o número de linha
+*Max(coluna) Maior valor da coluna
+**Min(coluna) Menor valor da coluna
+*Sum(coluna) Soma dos valores das colunas
+*/
+
+-- Qual é média de preço das pizzas?
+
+select avg(valor) as preco_medio from pizza;
+select avg(valor) as preco_medio from pizza p where nome like '%esa';
+
+-- Quantos sabores de pizza temos cadastrados?
+select count(*) as qtde from pizza; -- considera valores nullos 
+select count(valor) as qtde from pizza; -- não considera valores nullos 
+
+-- Qual a pizza mais cara?
+select max(valor) as maior_valor from pizza;
+
+-- Qual a pizza mais barata?
+select min(valor) as menor_valor from pizza;
+
+-- Somar todos os valores 
+select sum(valor) as soma from pizza;
+
+select sum(valor) as total from item_pedido where pedido_id = 7;
+
+select sum(quantidade * valor) from item_pedido where pedido_id = 7;
+
+select c.cliente_id, c.nome, cont(p.pedido_id) as qtde_pedido
+from pedido p
+inner join cliente c on p.cliente_id = c.cliente_id group by c.cliente_id , c.nome ; 
+
+
+
+
+
+
+
+
 
 
 
